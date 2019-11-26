c947b577e9e44bf596eccf79771108effe009948
Polina <polinaucc@gmail.com>
2019-11-26 17:01:32 +0200

    Added regexes for the future form's fields

---
+++ src/com/company/regex.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.util.regex.Pattern;
+
+public interface regex {
+    Pattern surnamePattern = Pattern.compile("([A-Z][a-z]{2,})|([А-ЩЇЮЯІ][а-щїюяіь']{2,})");
+    Pattern secondNamePattern = Pattern.compile("(^[А-ЩЇЮЯІ][а-щїюяіь']{2,})(ович$|івна$|ївна$)");
+    Pattern loginPattern = Pattern.compile("^[a-zA-Z][a-zA-Z0-9_]{1,20}$");
+    Pattern groupPattern = Pattern.compile("Group A|Group B");
+    Pattern mobilePhonePattern = Pattern.compile("[+][0-9]+[(]?[0-9]{3}[)]?[0-9]{3}[-]?[0-9]{3}");
+    Pattern emailPattern = Pattern.compile("(\\w{6,})@(\\w+\\.)([a-z]{2,4})");
+    Pattern skypePattern = Pattern.compile("^[\\w]{5,31}$");
+    Pattern indexPattern = Pattern.compile("[0-9]{5}");
+    Pattern cityNamePattern = Pattern.compile("([A-Z][a-z]+)|([А-ЩЇЮЯІ][а-щїюяіь']+)");
+    Pattern houseNumberPattern = Pattern.compile("[0-9]+[а-е]?");
+    Pattern roomNumberPattern = Pattern.compile("[0-9]+");
+    Pattern datePattern = Pattern.compile("(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\\d\\d");
+}
